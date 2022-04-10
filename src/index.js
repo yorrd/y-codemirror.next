8805efc555d3bbe7aca499fcd8c0712503dc1a47
yorrd <kai@adornis.de>
2022-04-10 14:25:04 +0200

    fix: allow undefined

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { yUndoManager, yUndoManagerFacet, YUndoManagerConfig, undo, redo, yUndoM
 export { yRemoteSelections, yRemoteSelectionsTheme, ySync, ySyncFacet, YSyncConfig, yUndoManagerKeymap }
 
 /**
- * @param {() => Y.Text} ytext
+ * @param {() => Y.Text | undefined} ytext
  * @param {any} awareness
  * @param {Object} [opts]
  * @param {Y.UndoManager | false} [opts.undoManager] Set undoManager to false to disable the undo-redo plugin
